60741d553cf7464dcb8c90b6596c8d4bbc9420d9
kareem <kareemwally@std.mans.edu.eg>
2024-08-11 06:15:41 +0300

    testing

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+
+"""
+a specified filr for testing the basemodel from which
+all models inherent
+class
+"""
+
+
+class TestBaseModel(unittest.TestCase):
+    """
+    testing the efficiency of the basemodel
+    """
+    def startup(self):
+        """
+        dealing with prerequisties of testing operation
+        """
+        pass
+
+    def test_save(self):
+        """
+        testing the save method of BaseModel
+        """
+        pass
+
+    def test_to_dict(self):
+        """
+        testing the to_dict method of BaseModel
+        """
+        pass
+
+    def teardown(self):
+        pass
